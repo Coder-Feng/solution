b5a00dc55d04b8b05d8bc2d36f0071e1d9ac6966
lock_free <fengxiaomailbox@163.com>
2017-03-15 16:00:25 +0800

    增加注释

--- src/main/java/com/alg/sort/QuickSort.java
+++ src/main/java/com/alg/sort/QuickSort.java
@@ -3,7 +3,7 @@ package com.alg.sort;
 /**
  * 
  * 〈一句话功能简述〉<br> 
- * 〈功能详细描述〉
+ * 〈快速排序实现〉
  *
  * @author coder_feng
  * @see [相关类/方法]（可选）
@@ -16,6 +16,17 @@ public class QuickSort implements Sort {
         sort(array, 0, array.length - 1);
     }
 
+    /**
+     * 
+     * 功能描述: <br>
+     * 将数组从小到大排序
+     *
+     * @param array
+     * @param bottom
+     * @param top
+     * @see [相关类/方法](可选)
+     * @since [产品/模块版本](可选)
+     */
     private void sort(int[] array, int bottom, int top) {
         if (bottom < top) {
             int divdePoint = partition(array, bottom, top);
@@ -25,10 +36,21 @@ public class QuickSort implements Sort {
 
     }
 
+    /**
+     * 
+     * 功能描述: <br>
+     * 将top所在是索引值作为一个基础值,将小于这个值的数据放在左边，大于这个值的放右边<br>
+     * 
+     *
+     * @param array
+     * @param bottom
+     * @param top
+     * @return
+     * @see [相关类/方法](可选)
+     * @since [产品/模块版本](可选)
+     */
     private int partition(int[] array, int bottom, int top) {
-
         int divideValue = array[top];
-
         int i = bottom;
         for (int loopIndex = bottom; loopIndex <= top; loopIndex++) {
             if (array[loopIndex] < divideValue) {
@@ -38,11 +60,9 @@ public class QuickSort implements Sort {
                 i++;
             }
         }
-
         int temp = array[top];
         array[top] = array[i];
         array[i] = temp;
-
         return i;
     }
 
